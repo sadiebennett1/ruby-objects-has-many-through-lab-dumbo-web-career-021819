3f08bb1ff0396a7d8730451194f487b32ea8a893
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-22 02:26:36 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,19 @@
 class Genre
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def songs
+  end 
+  
+  def artists
+end 
+
+end
