edde5761ec419936914e40072aae6940fd3fdb4b
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-22 02:24:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,13 @@
 class Song
   @@all = []
   
+  attr_reader :name, :artist, :genre 
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre 
+  end 
   
   def self.all
     @@all
