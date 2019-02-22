e4e9ccd323c93288899fb55c3af1f348889d425a
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-22 03:03:35 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor 
+  @@all = []
+  
+  attr_reader :name 
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def new_appointment
+  end
+  
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment. doctor variable = self 
+    end 
+  end
+  
+  def patients
+    appointments.map do |appointment|
+      appointment.patient 
+    end
+end
